f5a22ce2f8eb42ef10ef927262def0b580eaf3aa
Azhaghiri <ganadhan6528@gmail.com>
2024-10-18 19:19:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,28 +10,23 @@ function convertToRoman(num) {
     };
 
   //your code here
-	let roman = ''; // This will hold the resulting Roman numeral
+	let roman = ''; 
 
-  // Iterate through the object by its keys (from index 0 to 6)
+
   for (let i = 0; i <= 6; i++) {
-    let symbol = obj[i][0]; // Roman numeral symbol
-    let value = obj[i][1];  // Corresponding value
+    let symbol = obj[i][0]; 
+    let value = obj[i][1];  
     
-    // Add the current Roman numeral to the result string while num >= value
     while (num >= value) {
-      roman += symbol; // Append the symbol to the Roman numeral string
-      num -= value;    // Subtract the value from num
+      roman += symbol;
+      num -= value;
     }
   }
 
   return roman;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
  console.log(convertToRoman(36));
-
- 
-
 
 // do not edit below this line
 module.exports = convertToRoman
